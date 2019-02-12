854c7d81eaf45ad325fd94cff0ae859945147da2
Mikheil Gigauri <mikheil.gigauri96@gmail.com>
2019-02-12 14:15:10 +0400

    Registration Form Started

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,8 +1,32 @@
-let x = 5,
-    y = 7;
-
-const concat = () => {
-    return +x+y
+const Login = (action) => {
+    const loginPopup = document.getElementById('loginPopup');
+    
+    if(action == 'open'){
+        loginPopup.style.display = 'flex';
+        setTimeout(() => { 
+            loginPopup.style.opacity = "1";
+        }, 100)
+    }
+    if(action == 'close'){
+        loginPopup.style.opacity = "0";
+        setTimeout(() => {
+            loginPopup.style.display = 'none';
+        }, 100)
+    }
 }
-
-console.log(concat());
+const Registration = (action) => {
+    const registrationPopup = document.getElementById('registrationPopup');
+    
+    if(action == 'open'){
+        registrationPopup.style.display = 'flex';
+        setTimeout(() => { 
+            registrationPopup.style.opacity = "1";
+        }, 100)
+    }
+    if(action == 'close'){
+        registrationPopup.style.opacity = "0";
+        setTimeout(() => {
+            registrationPopup.style.display = 'none';
+        }, 100)
+    }
+}
